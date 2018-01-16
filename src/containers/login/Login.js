fb4cde3da732f3a06bd756a5e38fb0c09b7306ed
v.mozharovsky <v.mozharovsky@gmail.com>
2018-01-16 20:24:24 +0200

    changing Login component

---
+++ src/containers/login/Login.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+import styles from "./loginStyles"
+// import {login} from '../../actions/userActions';
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            password: '',
+            loading: false,
+            rememberMe: false,
+            showError : null
+
+        }
+
+    }
+    componentWillMount(){
+        if(localStorage.getItem('token')){
+            this.props.history.push(`/devices`);
+        }
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+    login(e){
+        e.preventDefault();
+        this.props.dispatch(login({user : this.state.userName, password: this.state.password}))
+
+    }
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.user.user){
+            if(this.state.rememberMe){
+                localStorage.setItem('token', nextProps.user.ssid);
+            }
+            this.props.history.push(`/devices`);
+        }
+        if(nextProps.user.error){
+            this.setState({
+                showError : nextProps.user.error
+            });
+            this.timeout = setTimeout(()=> {
+                    this.setState({
+                        showError:null
+                    })
+                }, 3000)
+
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.loginWrap}>
+                <View style={styles.loginFormWrap}>
+
+                    {/*<h3><img src={require('../../images/minifinder-tracktor-logo-login.png')} alt=""/></h3>*/}
+                    {/*<form onSubmit={(e)=>this.login(e)}>*/}
+                        {/*<label htmlFor="userName" className="login-form-label">*/}
+                            {/*<div className='login_bg'>*/}
+                                {/*<img src={require('../../images/user.svg')} alt=""/>*/}
+                            {/*</div>*/}
+
+                            {/*<input type="text" value={this.state.userName}*/}
+                                   {/*name="userName"*/}
+                                   {/*className="login-form-input"*/}
+                                   {/*onChange={(e) => this.handleInputChange(e)}*/}
+                                   {/*autoFocus={true}*/}
+                            {/*/>*/}
+                        {/*</label>*/}
+                        {/*<label htmlFor="password" className="login-form-label">*/}
+                            {/*<div className='login_bg'>*/}
+                                {/*<img src={require('../../images/password.svg')} alt=""/>*/}
+                            {/*</div>*/}
+                            {/*<input type="password"*/}
+                                   {/*name="password"*/}
+                                   {/*className="login-form-input"*/}
+                                   {/*value={this.state.password}*/}
+                                   {/*onChange={(e) => this.handleInputChange(e)}/>*/}
+                        {/*</label>*/}
+
+                        {/*<button type="submit" className="login-submit-button text-sd">{this.props.user.loading?'Loading' : 'Login'}</button>*/}
+                        {/*{this.state.showError?<p>{this.state.showError}</p>:null}*/}
+                        {/*<div className='login-form-checkbox'>*/}
+                            {/*<input type="checkbox" id="rememberMe" name="rememberMe" checked={this.state.rememberMe}*/}
+                                   {/*onChange={(e) => this.handleInputChange(e)}/>*/}
+                            {/*<label htmlFor="rememberMe">*/}
+
+                            {/*</label>*/}
+                            {/*<p>Remember me</p>*/}
+                        {/*</div>*/}
+
+                    {/*</form>*/}
+                </View>
+            </View>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const {user} = state;
+    return { user}
+}
+
+export default connect(mapStateToProps)(Login);
---
+++ src/containers/login/loginStyles.js
@@ -0,0 +1,14 @@
+const styles = StyleSheet.create({
+  loginWrap: {
+    height:'100%',
+    backgroundImage: 'url("../../images/bg_home.png")'
+  },
+  loginFormWrap: {
+    maxWidth: '500px',
+    margin: 0% auto,
+    padding: '15% 0',
+    textAlign: center
+  },
+});
+
+export default styles
