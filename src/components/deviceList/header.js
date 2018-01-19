55ed9883a072140d842c0aa3d6b134f1a0e41b69
v.mozharovsky <v.mozharovsky@gmail.com>
2018-01-19 18:13:48 +0200

    header added

--- src/components/deviceList/header.js
+++ src/components/deviceList/header.js
@@ -1,31 +1,43 @@
 import React, {Component} from 'react';
-import '../../containers/deviceList/DeviceList.css';
-import FontAwesome from 'react-fontawesome';
+// import '../../containers/deviceList/DeviceList.css';
+import { Image } from "react-native"
+import { Header } from 'react-native-elements'
 
-const Header = ({toggleList, logout})=> {
+const HeaderWrap = ({toggleList, logout})=> {
 
 
         return (
-            <div className="bar-wrapper top">
-                <div className='header-left'>
-                    <FontAwesome
-                    onClick={()=>toggleList()}
-                    className='icon-color'
-                    name='bars'
-                    style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)'}}
-                    />
-                </div>
-                <p className='header-mid'><img className="header-logo" src={require("../../images/minifinder-tracktor-logo-login.png")}/></p>
-                <div className='header-right'>
-                    <FontAwesome
-                    className='icon-color'
-                    name='sign-out'
-                    style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-                    onClick={()=>{logout()}}
-                    />
-                </div>
-            </div>
+          <Header
+            outerContainerStyles={{backgroundColor: 'black'}}
+            leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={<Image source={require("../../images/minifinder-tracktor-logo-login.png")}
+                                    style={{width: 220, height: 30, top: 2, resizeMode: 'contain'}}/>}
+            rightComponent={{ icon: 'exit-to-app', color: '#fff' }}
+          />
+
+            // <View className="bar-wrapper top">
+            //     <View className='header-left'>
+            //         <FontAwesome
+            //         onClick={()=>toggleList()}
+            //         className='icon-color'
+            //         name='bars'
+            //         style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)'}}
+            //         />
+            //     </View>
+            //     <p className='header-mid'>
+            //       <Image style={styles.headerLogo}
+            //            src={require("../../images/minifinder-tracktor-logo-login.png")}/>
+            //     </p>
+            //     <div className='header-right'>
+            //         <FontAwesome
+            //         className='icon-color'
+            //         name='sign-out'
+            //         style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+            //         onClick={()=>{logout()}}
+            //         />
+            //     </div>
+            // </View>
         );
 }
 
-export default Header;
+export default HeaderWrap;
